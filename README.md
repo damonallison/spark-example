611411d9389aa12b10403596747958d32e568732
Damon Allison <damon@shipt.com>
2022-08-17 08:26:06 -0500

    Spark SQL

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # spark-example
 
-A spark example:
+Example `spark` code.
+
+Spark's main abstraction is the `DataSet` (previously `RDD`). `DataSet`s support
+two types of operations: `transformations` and `actions`.
+
+Transformations (i.e., map, filter) are done in parallel on the cluster.
+Transformations are lazy and not evaluated until an `action` is performed (i.e.,
+count, collect). When an action is performed, the transformations occur on the
+cluster and the results are delivered to the driver.
+
+Functions that run in the cluster should be functional (do not modify external
+state or close over variables that can mutate). Closures (functions + required
+state) are copied to each cluster node. Thus, each node operates on a different
+copy of closed over variables.
+
+Accumulators are variables that can be safely shared among cluster nodes.
+
+
+## Spark SQL / DataFrame / Dataset API
+
+Spark SQL and the Dataset API are meant to do structured data processing (think
+Pandas). Both the SQL and Dataset APIs use the same underlying execution engine
+to process data.
+
+IMPORTANT: Python does not support the Dataset API.
+
+
 
 ```shell
 # Install pyspark as a pip package
@@ -8,3 +34,13 @@ poetry add pyspark
 
 poetry run pyspark
 ```
+
+
+## Databricks
+
+"Lakehouse": A combination of ACID transactions and metadata of databases with
+data stored in parquet files.
+
+* Metadata (table names, schemas, etc) is stored in a "metastore".
+* Table metadata and data are saved in cloud storage.
+
--- main.py
+++ main.py
@@ -1,18 +1,64 @@
+import platform
+import sys
+
 from pyspark.sql import SparkSession
 from pyspark.sql import functions
 
-spark = SparkSession.builder.appName("spark-example").getOrCreate()
 
-df = spark.read.text("README.md")
+def print_separator() -> None:
+    print("-".join([""] * 80))
 
-# Filter creates a new df with the given predicate
-# TODO: how do predicates work?
-num_spark = df.filter(df.value.contains("spark")).count()
-num_example = df.filter(df.value.contains("example")).count()
 
-print(f"Totla lines {df.count()}")
-print(f"Lines w/ spark {num_spark}")
-print(f"Lines w/ example {num_example}")
+def print_environment(spark: SparkSession) -> None:
+    """Spark 3.3.0 supports Python 3.7 and above."""
+    print_separator()
+    print(f"System version: {sys.version}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Spark version: {spark.version}")
+    print_separator()
 
-print(f"Lines with spark: {df.filter(df.value.contains('spark')).collect()}")
-spark.stop()
+
+if __name__ == "__main__":
+    #
+    # Spark 2.0 introduced a new entry point called SparkSession, replacing
+    # SparkContext, SQLContext and HiveContext as the main Spark entry point.
+    #
+    with SparkSession.builder.appName("spark-example").getOrCreate() as spark:
+        print_environment(spark)
+
+        # NOTE: This "json" file isn't vaid json. Each line contains a JSON object.
+        df = spark.read.json("./datasets/people.json.txt")
+
+        df.printSchema()
+
+        # Select and print a few columns
+        df.select("name", "age").show()
+
+        # Boolean masking
+        df.filter(df["age"] > 18).show()
+
+        # Group
+        df.groupBy("age").count().show()
+
+        #
+        # SQL
+        #
+
+        #
+        # Registering a DataFrame as a temporary view allows you to run SQL
+        # queries over its data. You can register DFs as session specific or
+        # global temp views.
+        #
+        # Temporary views are session specific. Global temporary views are kept
+        # alive until the Spark application terminates.
+        #
+        # Global temp views are created in a system specific database called
+        # "global_temp"
+        #
+        df.createOrReplaceGlobalTempView("people")
+
+        sqlDf = spark.sql(
+            "select * from global_temp.people where age is not null and age > 18"
+        )
+        sqlDf.printSchema()
+        sqlDf.show()
